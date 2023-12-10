dc5eeca224ad261c2d43b7b193ebf0fe70e5677d
Clément Vassant <clement@Clements-MBP.home>
2023-12-10 16:03:04 +0100

    My solution to exercise 10

--- exercise/day10/src/main/java/games/FizzBuzz.java
+++ exercise/day10/src/main/java/games/FizzBuzz.java
@@ -1,12 +1,14 @@
 package games;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static java.util.Arrays.stream;
+
 public class FizzBuzz {
     public static final int MIN = 0;
     public static final int MAX = 100;
-    public static final int FIZZ = 3;
-    public static final int BUZZ = 5;
-    public static final int FIZZBUZZ = 15;
-
     private FizzBuzz() {
     }
 
@@ -14,27 +16,37 @@ public class FizzBuzz {
         if (isOutOfRange(input)) {
             throw new OutOfRangeException();
         }
-        return convertSafely(input);
-    }
-
-    private static String convertSafely(Integer input) {
-        if (is(FIZZBUZZ, input)) {
-            return "FizzBuzz";
-        }
-        if (is(FIZZ, input)) {
-            return "Fizz";
-        }
-        if (is(BUZZ, input)) {
-            return "Buzz";
-        }
-        return input.toString();
-    }
-
-    private static boolean is(Integer divisor, Integer input) {
-        return input % divisor == 0;
+        return FizzBuzzConditions.convert(input);
     }
 
     private static boolean isOutOfRange(Integer input) {
         return input <= MIN || input > MAX;
     }
+
+    private enum FizzBuzzConditions{
+        FIZZBUZZ((number) -> is(15, number), (number)-> "FizzBuzz"),
+        FIZZ((number) -> is(3, number),(number)-> "Fizz"),
+        BUZZ((number) -> is(5, number), (number)-> "Buzz"),
+        DEFAULT((number) -> true, Object::toString);
+
+        private final Predicate<Integer> condition;
+        private final Function<Integer,String> converter;
+
+        FizzBuzzConditions(Predicate<Integer> condition, Function<Integer, String> converter) {
+            this.condition = condition;
+            this.converter = converter;
+        }
+
+        public static String convert(Integer number){
+            return stream(FizzBuzzConditions.values())
+                    .filter(it -> it.condition.test(number))
+                    .findFirst()
+                    .map(it -> it.converter.apply(number))
+                    .get();
+        }
+        private static boolean is(Integer divisor, Integer input) {
+            return input % divisor == 0;
+        }
+    }
 }
+
